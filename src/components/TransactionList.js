ac440324257d195ea47341a2aaff6cca805614a5
gouhar1512 <ushran987@gmail.com>
2023-09-14 02:38:24 +0530

    Created TransactionContext, intialState and provided initialState

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { Transaction } from "./Transaction";
+import { TransactionContext } from "../context/TransactionContext";
 
 export const TransactionList = () => {
-  // const { transactions } = useContext();
-
+  const { transactions } = useContext(TransactionContext);
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {/* {transactions.map((transaction) => (
+        {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
-        ))} */}
+        ))}
       </ul>
     </>
   );
---
+++ src/context/TransactionContext.js
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+
+const initialState = {
+  transactions: [
+    {
+      id: Math.floor(Math.random() * 100000000),
+      text: "Milk",
+      amount: 10,
+    },
+    {
+      id: Math.floor(Math.random() * 100000000),
+      text: "Bread",
+      amount: 20,
+    },
+    {
+      id: Math.floor(Math.random() * 100000000),
+      text: "Butter",
+      amount: 5,
+    },
+  ],
+};
+
+export const TransactionContext = createContext(initialState);
